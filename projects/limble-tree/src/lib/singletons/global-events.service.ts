fe9d7236df6e5590855772cd0a557e9e4bc73d59
derekparsons718 <derekparsons718@gmail.com>
2021-03-03 22:50:18 -0700

    Scrolling performance improvement

--- projects/limble-tree/src/lib/singletons/global-events.service.ts
+++ projects/limble-tree/src/lib/singletons/global-events.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, NgZone } from "@angular/core";
 import { fromEvent, Observable, Subscription } from "rxjs";
 import { filter, throttleTime } from "rxjs/operators";
 
@@ -35,104 +35,113 @@ function getScrollParent(element: HTMLElement): HTMLElement {
 
 @Injectable()
 export class GlobalEventsService {
-   private globalDrag$: Observable<DragEvent>;
+   private globalDrag$: Observable<DragEvent> | undefined;
    private globalDragSubscription: Subscription | undefined;
    public scrolling: boolean;
 
-   constructor() {
-      this.globalDrag$ = fromEvent<DragEvent>(document, "drag");
+   constructor(private readonly ngZone: NgZone) {
+      this.ngZone.runOutsideAngular(() => {
+         this.globalDrag$ = fromEvent<DragEvent>(document, "drag");
+      });
       this.scrolling = false;
    }
 
    public addScrolling() {
-      if (this.globalDragSubscription !== undefined) {
-         return;
-      }
-      let viewPortHeight: number;
-      let scrollAreaSize: number;
-      let edgeTop: number;
-      let edgeBottom: number;
-      let isInTopScrollArea: boolean;
-      let isInBottomScrollArea: boolean;
-      let timer: ReturnType<typeof setTimeout>;
-      let scrollableDiv: HTMLElement | null;
-      let relativeY: number;
-      this.globalDragSubscription = this.globalDrag$
-         .pipe(
-            throttleTime(25),
-            filter((event) => {
-               if (!event.target) {
-                  return false;
+      this.ngZone.runOutsideAngular(() => {
+         if (this.globalDragSubscription !== undefined) {
+            return;
+         }
+         if (this.globalDrag$ === undefined) {
+            throw new Error("Could not get observable");
+         }
+         let viewPortHeight: number;
+         let scrollAreaSize: number;
+         let edgeTop: number;
+         let edgeBottom: number;
+         let isInTopScrollArea: boolean;
+         let isInBottomScrollArea: boolean;
+         let timer: ReturnType<typeof setTimeout>;
+         let scrollableDiv: HTMLElement | null;
+         let relativeY: number;
+         this.globalDragSubscription = this.globalDrag$
+            .pipe(
+               throttleTime(25),
+               filter((event) => {
+                  if (!event.target) {
+                     return false;
+                  }
+                  scrollableDiv = getScrollParent(event.target as HTMLElement);
+                  viewPortHeight = scrollableDiv.clientHeight;
+                  const viewPortWidth = scrollableDiv.clientWidth;
+                  let relativeX: number;
+                  if (
+                     window
+                        .getComputedStyle(scrollableDiv)
+                        .getPropertyValue("position")
+                        .toLowerCase() === "fixed"
+                  ) {
+                     relativeX = event.clientX;
+                     relativeY = event.clientY;
+                  } else {
+                     const boundingRect = scrollableDiv.getBoundingClientRect();
+                     const scrollableDivAncestor = getScrollParent(
+                        scrollableDiv
+                     );
+                     relativeX =
+                        event.clientX -
+                        (boundingRect.left + scrollableDivAncestor.scrollLeft);
+                     relativeY =
+                        event.clientY -
+                        (boundingRect.top + scrollableDivAncestor.scrollTop);
+                  }
+                  if (
+                     relativeX < 0 ||
+                     relativeX > viewPortWidth ||
+                     relativeY < 0 ||
+                     relativeY > viewPortHeight
+                  ) {
+                     //Outside of scrollable div
+                     return false;
+                  }
+                  scrollAreaSize = Math.max(viewPortHeight * 0.1, 100);
+                  edgeTop = scrollAreaSize;
+                  edgeBottom = viewPortHeight - scrollAreaSize;
+                  isInTopScrollArea = relativeY < edgeTop;
+                  isInBottomScrollArea = relativeY > edgeBottom;
+                  return isInTopScrollArea || isInBottomScrollArea;
+               })
+            )
+            .subscribe(() => {
+               if (scrollableDiv === null) {
+                  return;
                }
-               scrollableDiv = getScrollParent(event.target as HTMLElement);
-               viewPortHeight = scrollableDiv.clientHeight;
-               const viewPortWidth = scrollableDiv.clientWidth;
-               let relativeX: number;
-               if (
-                  window
-                     .getComputedStyle(scrollableDiv)
-                     .getPropertyValue("position")
-                     .toLowerCase() === "fixed"
-               ) {
-                  relativeX = event.clientX;
-                  relativeY = event.clientY;
+               const height = scrollableDiv.scrollHeight;
+               const maxScrollY = height - viewPortHeight;
+               const currentScrollY = scrollableDiv.scrollTop;
+               const canScrollUp = currentScrollY > 0;
+               const canScrollDown = currentScrollY < maxScrollY;
+               let nextScrollY: number;
+               const maxStep = 75;
+               if (isInTopScrollArea && canScrollUp) {
+                  const intensity = (edgeTop - relativeY) / scrollAreaSize;
+                  nextScrollY = currentScrollY - maxStep * intensity;
+               } else if (isInBottomScrollArea && canScrollDown) {
+                  const intensity = (relativeY - edgeBottom) / scrollAreaSize;
+                  nextScrollY = currentScrollY + maxStep * intensity;
                } else {
-                  const boundingRect = scrollableDiv.getBoundingClientRect();
-                  const scrollableDivAncestor = getScrollParent(scrollableDiv);
-                  relativeX =
-                     event.clientX -
-                     (boundingRect.left + scrollableDivAncestor.scrollLeft);
-                  relativeY =
-                     event.clientY -
-                     (boundingRect.top + scrollableDivAncestor.scrollTop);
+                  return;
                }
-               if (
-                  relativeX < 0 ||
-                  relativeX > viewPortWidth ||
-                  relativeY < 0 ||
-                  relativeY > viewPortHeight
-               ) {
-                  //Outside of scrollable div
-                  return false;
+               nextScrollY = Math.max(0, Math.min(maxScrollY, nextScrollY));
+               if (nextScrollY !== currentScrollY) {
+                  scrollableDiv.scrollTo({ top: nextScrollY });
+                  this.scrolling = true;
+                  clearTimeout(timer);
+                  timer = setTimeout(() => {
+                     this.scrolling = false;
+                  }, 100);
                }
-               scrollAreaSize = Math.max(viewPortHeight * 0.1, 100);
-               edgeTop = scrollAreaSize;
-               edgeBottom = viewPortHeight - scrollAreaSize;
-               isInTopScrollArea = relativeY < edgeTop;
-               isInBottomScrollArea = relativeY > edgeBottom;
-               return isInTopScrollArea || isInBottomScrollArea;
-            })
-         )
-         .subscribe(() => {
-            if (scrollableDiv === null) {
-               return;
-            }
-            const height = scrollableDiv.scrollHeight;
-            const maxScrollY = height - viewPortHeight;
-            const currentScrollY = scrollableDiv.scrollTop;
-            const canScrollUp = currentScrollY > 0;
-            const canScrollDown = currentScrollY < maxScrollY;
-            let nextScrollY: number;
-            const maxStep = 75;
-            if (isInTopScrollArea && canScrollUp) {
-               const intensity = (edgeTop - relativeY) / scrollAreaSize;
-               nextScrollY = currentScrollY - maxStep * intensity;
-            } else if (isInBottomScrollArea && canScrollDown) {
-               const intensity = (relativeY - edgeBottom) / scrollAreaSize;
-               nextScrollY = currentScrollY + maxStep * intensity;
-            } else {
-               return;
-            }
-            nextScrollY = Math.max(0, Math.min(maxScrollY, nextScrollY));
-            if (nextScrollY !== currentScrollY) {
-               scrollableDiv.scrollTo({ top: nextScrollY });
-               this.scrolling = true;
-               clearTimeout(timer);
-               timer = setTimeout(() => {
-                  this.scrolling = false;
-               }, 100);
-            }
-         });
+            });
+      });
    }
 
    public removeScrolling() {
