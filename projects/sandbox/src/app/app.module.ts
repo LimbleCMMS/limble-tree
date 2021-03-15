8b93be3db5472742f7ac6f45f67a9e2bf3672d33
derekparsons718 <derekparsons718@gmail.com>
2021-03-15 15:02:05 -0600

    A couple fixes to previous

--- projects/sandbox/src/app/app.module.ts
+++ projects/sandbox/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { LimbleTreeModule } from "limble-tree";
+import { LimbleTreeModule } from "@limble/limble-tree";
 import { TreeItemComponent } from "./tree-item/tree-item.component";
 import { TreeItemAltComponent } from "./tree-item-alt/tree-item-alt.component";
 
