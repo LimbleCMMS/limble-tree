678657396b38a83f9b4923a835c21a20d56fcb22
DerekLimble <derek.parsons@limblecmms.com>
2023-03-10 16:08:38 -0700

    fix sandbox test

--- projects/sandbox/src/app/app.component.spec.ts
+++ projects/sandbox/src/app/app.component.spec.ts
@@ -6,8 +6,7 @@ import { AppComponent } from "./app.component";
 describe("AppComponent", () => {
    beforeEach(async () => {
       await TestBed.configureTestingModule({
-         imports: [RouterTestingModule, LimbleTreeModule],
-         declarations: [AppComponent]
+         imports: [RouterTestingModule, LimbleTreeModule, AppComponent]
       }).compileComponents();
    });
 
